547500e2a236ef10a743e3a2ad0a7c39ceda5c8f
Snegirev Anfim Alexandrovich <anfim.sn@gmail.com>
2022-09-22 16:05:07 +0300

    Notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+  element
 
+  constructor (message = '', {duration = 2000, type = 'success'} = {}) {
+    this.message = message
+    this.duration = duration
+    this.type = type
+
+    this.render();
+  }
+
+  render () {
+    if (this.message.length !== 0) {
+      const element = document.createElement("div");
+      element.innerHTML = this.template;
+      this.element = element.firstElementChild;
+    }
+  }
+
+
+  get template() {
+    return `
+    <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  show (div = {}) {
+    document.querySelector('.notification')?.remove()
+
+    if (div instanceof HTMLElement) {
+      div.append(this.element);
+    } else {
+      document.body.append(this.element);
+    }
+
+    setTimeout(() => {
+      this.remove()
+    }, this.duration);
+  }
+
+  remove () {
+    this.element?.remove()
+  }
+
+  destroy () {
+    this.remove()
+    this.element = null
+  }
 }
