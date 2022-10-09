4223d70f171379fc1c8c9fc3ba5e946fa83e4c07
Snegirev Anfim Alexandrovich <anfim.sn@gmail.com>
2022-10-09 17:41:53 +0400

    Add double-slider, range-picker, product-form v2

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -10,12 +10,23 @@ export default class SortableList {
   render() {
     const element = document.createElement("ul");
     element.classList.add('sortable-list')
-    for (let i = 0; i < this.items.length; i++) {
-      const item = this.items[i];
-      item.classList.add('sortable-list__item')
-      element.append(item)
+    
+    for (const item of this.items) {
+      const elem = document.createElement('div')
+      elem.innerHTML = item
+      // elem.classList.add('sortable-list__item')
+      element.append(elem.firstElementChild)
     }
+
     this.element = element;
+  }
+
+  createPlaceholder() {
+    const placeholder = document.createElement('div')
+    placeholder.style.width = '100%'
+    placeholder.style.height = `${this.itemHeight}px`
+    placeholder.classList.add('sortable-list__placeholder')
+    this.placeholder = placeholder
   }
 
   initEventListeners () {
@@ -27,80 +38,80 @@ export default class SortableList {
     const target = event.target.closest('[data-delete-handle]')
     if (target === null) return
 
-    target.parentNode.remove()
+    target.closest('.products-edit__imagelist-item').remove()
   }
 
   handleDrag = event => {
     const target = event.target.closest('[data-grab-handle]')
     if (target === null) return
 
-    const parent = target.parentNode
-    const container = parent.parentNode
+    this.item = target.closest('.products-edit__imagelist-item')
+    this.itemHeight = this.item.clientHeight
+    this.itemWidth = this.item.clientWidth
+    this.index = Array.from([...this.element.children]).indexOf(this.item);
 
-    const itemHeight = parent.clientHeight
-    const containerHeight = container.clientHeight
+    this.containerTop = this.element.getBoundingClientRect().top
+    this.containerBottom = this.element.getBoundingClientRect().bottom
 
-    const containerTop = container.getBoundingClientRect().top
-    const containerBottom = container.getBoundingClientRect().bottom
+    this.shiftX = event.clientX - this.item.getBoundingClientRect().left;
+    this.shiftY = event.clientY - this.item.getBoundingClientRect().top;
+    
+    this.createPlaceholder()
 
-    const placeholder = document.createElement('div')
-    placeholder.style.width = '100%'
-    placeholder.style.height = `${itemHeight}px`
-    placeholder.classList.add('sortable-list__placeholder')
+    this.item.classList.add('sortable-list__item_dragging')
+    this.item.style.width = this.itemWidth + 'px' 
+    this.element.append(this.item);
+    this.element.children[this.index].before(this.placeholder)
 
-    let shiftX = event.clientX - parent.getBoundingClientRect().left;
-    let shiftY = event.clientY - parent.getBoundingClientRect().top;
+    this.moveOn(event, this.shiftX, this.shiftY)
 
-    let indexArr = [0, 0]
-    let index = Array.from([...container.children]).indexOf(parent);
+    this.indexArr = [0, 0]
 
-    parent.classList.add('sortable-list__item_dragging')
-    parent.style.width = '100%'
-    this.element.append(parent);
-    container.children[index].before(placeholder)
-    moveAt(event.pageX, event.pageY);
-  
-    function moveAt(pageX, pageY) {
-      parent.style.left = pageX - shiftX + 'px';
-      parent.style.top = pageY - shiftY + 'px';
-    }
+    document.addEventListener('mousemove', this.onMouseMove);
 
-    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', this.onMouseUp);
+  }
 
-    function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY);
-      if (event.pageY < containerTop + itemHeight / 2) {
-        container.prepend(placeholder)
-      } else if (event.pageY > containerBottom - itemHeight / 2) {
-        container.append(placeholder)
-      } else {
-        for (let i = containerTop; i < containerBottom; i += itemHeight) {
-          if (event.pageY > i && event.pageY < (i + itemHeight)) {
-            console.log(i);
-            indexArr.push(i)
-            indexArr.shift()
-            if ((indexArr[0]) - (indexArr[1]) > 0) {
-              index--
-            } else if ((indexArr[0]) - (indexArr[1]) < 0) {
-              index++
-            }
-            if (index < 1 || index > containerHeight / itemHeight - 2) return
-            container.children[index].before(placeholder)
+  onMouseMove = event => {
+    this.moveOn(event, this.shiftX, this.shiftY)
+
+    if (event.pageY < this.containerTop + this.itemHeight / 2) {
+      this.element.prepend(this.placeholder)
+    } else if (event.pageY > this.containerBottom - this.itemHeight / 2) {
+      this.element.append(this.placeholder)
+    } else {
+      for (let i = this.containerTop; i < this.containerBottom; i += this.itemHeight) {
+        if (event.pageY > i && event.pageY < (i + this.itemHeight)) {
+          this.indexArr.push(i)
+          this.indexArr.shift()
+          if ((this.indexArr[0]) - (this.indexArr[1]) === this.itemHeight) {
+            this.index--
+          } else if ((this.indexArr[0]) - (this.indexArr[1]) === -this.itemHeight) {
+            this.index++
           }
+          if (this.index < 1 || this.index > this.items.length - 1) return
+          this.element.children[this.index].before(this.placeholder)
         }
       }
     }
-
-    parent.onmouseup = function() {
-      document.removeEventListener('mousemove', onMouseMove);
-      parent.onmouseup = null;
-      placeholder.replaceWith(parent)
-      parent.classList.remove('sortable-list__item_dragging')
-      parent.style = null
-    };
+  }
+  
+  moveOn(event, shiftX, shiftY) {
+    this.item.style.left = event.pageX - shiftX + 'px';
+    this.item.style.top = event.pageY - shiftY + 'px';
   }
 
-  
+  onMouseUp = event => {
+    document.removeEventListener('mousemove', this.onMouseMove);
+    this.item.onmouseup = null;
+    this.placeholder.replaceWith(this.item)
+    this.item.classList.remove('sortable-list__item_dragging')
+    this.item.style = null
+  };
+
+  getLiElments(elem) {
+    console.log(elem);
+  }
 
   remove () {
     this.element?.remove()
